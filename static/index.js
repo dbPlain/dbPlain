78cbe03fd6be67266dc3ea4df4d4ebdf8e095821
Lorenzo Lunghi <lunghi.1888116@studenti.uniroma1.it>
2022-05-01 16:28:25 +0200

    Setup of the environment

---
+++ static/index.js
@@ -0,0 +1,23 @@
+function check(){
+    var x = getCookie('email');
+    console.log(x);
+    if (x){
+        document.getElementById("bho1").style.display="none";
+        document.getElementById("bho2").style.display="none";
+        document.getElementById("bho3").style.display="run-in";
+    } else {
+        document.getElementById("bho1").style.display="run-in";
+        document.getElementById("bho2").style.display="run-in";
+        document.getElementById("bho3").style.display="none";
+    }
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
