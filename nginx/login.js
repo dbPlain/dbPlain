6ab4e40edfc7c5e1aaac14d368029550035ad618
Paolo Marchignoli <Paolo.marchignoli2000@gmail.com>
2022-05-19 21:44:20 +0200

    pulizia codice

--- nginx/login.js
+++ nginx/login.js
@@ -1,5 +1,5 @@
 function valida() {
-    // "checked" proprietà booleana particolare delle checkbox
+    
     if (document.getElementById("rmb").checked) {
         alert("Hai scelto di essere ricordato come uno stronzo");
     }
@@ -8,31 +8,7 @@ function valida() {
     }
 }
 
-// triggerata solo quando si clicca sul tasto
-/*function setCookie(name, value, options = {}) {
 
-    options = {
-        path: '/',
-        // aggiungi altri percorsi di default se necessario
-        ...options
-    };
-    
-    if (options.expires instanceof Date) {
-        options.expires = options.expires.toUTCString();
-    }
-    
-    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + "; domain=localhost";
-    
-    for (let optionKey in options) {
-        updatedCookie += "; " + optionKey;
-        let optionValue = options[optionKey];
-        if (optionValue !== true) {
-        updatedCookie += "=" + optionValue;
-        }
-    }
-    
-    document.cookie = updatedCookie;
-}*/
 
 
 function checkAuth() {
@@ -40,10 +16,7 @@ function checkAuth() {
 }
 
 
-// miglioramento: settare tutti i cookie qua (prendendo i valori dagli elementi html della pagina), tanto il login è sempre lo stesso.
-// inserire httponly e :
-// " will be soon rejected because it has the “SameSite” attribute set to “None” or an invalid value, without the “secure” attribute. 
-// To know more about the “SameSite“ attribute, read https://developer.mozilla.org/docs/Web/HTTP/Headers/Set-Cookie/SameSite "
+
 function setCookie(name,value,days) {
     var expires = "";
     if (days) {
