a46cb6e4241aa6b3cefd1180a02144900ad0214b
Lorenzo Lunghi <lunghi.1888116@studenti.uniroma1.it>
2022-07-18 15:49:49 +0200

    pulizia

--- server/app.js
+++ server/app.js
@@ -1,29 +1,46 @@
+var request = require('request');
+var express = require('express');
+const app = express();
+const { Pool } = require('pg');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const fs = require('fs');
 var XLSX = require("xlsx");
 var path = require('path');
 
 let googleOAuth = require('./googleOAuth');
-// Serving static files from "public" folder (documentazione api interne)
+let googleDrive = require('./googleDrive')
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+app.use(compression());
 
-var request = require('request');
-var express = require('express');
-const app = express();
-app.use(express.static(path.join(__dirname, 'public/')));
-const { Pool } = require('pg');
+const couchdb = require('nano')('http://admin:admin@' + process.env.COUCHDB_URL + '');
+const utente = couchdb.db.use('utente');
+
 const http = require ('http')
-var bodyParser = require('body-parser');
 var server = http.createServer(app);
 const io = require('socket.io')(server);
 
+// Serving static files from "public" folder (documentazione api interne, '/apidoc')
+app.use(express.static(path.join(__dirname, 'public/')));
 
-// WEB SOCKET 
+
+
+
+/********************************************/
+/*				  WEB SOCKET	 		    */
+/********************************************/
 
 io.on("connection",async(socketEnt) => {
    
   socketEnt.on("disconnect", (arg) =>{
-	
 	console.log("disconnesso")
   })
+
   socketEnt.on("select-dati", (arg) =>{
 	console.log(arg)
 	var datiDB = JSON.parse(arg);
@@ -104,31 +121,14 @@ io.on("connection",async(socketEnt) => {
 		}
 	});
 	
-
-	
-	
   })
 })
 
 
-app.use(bodyParser.urlencoded({ extended: false }));
-const couchdb = require('nano')('http://admin:admin@' + process.env.COUCHDB_URL + '');
-app.use(bodyParser.json());
 
-let googleDrive = require('./googleDrive')
-const compression = require('compression');
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
-app.use(compression());
-
-//const persona = couchdb.db.use("persona")
-const utente = couchdb.db.use('utente');
-
-var self = this
-
-
-
-/*** API ESTERNA Google Drive ***/
+/********************************************/
+/*		API esterna: Google Drive	 	    */
+/********************************************/
 
 app.get('/salva/tabella', function(req,res){
 
@@ -191,21 +191,20 @@ app.get('/uso_token/salvataggio/gdrive', googleDrive.Salva_su_Google_Drive, asyn
 		}
 	})
 
-	// res.redirect('/static/home/') // si bagga spesso la scritta dropdown 'tabelle'
+	res.redirect('/static/home/') // si bagga spesso la scritta dropdown 'tabelle'
 	// URL verso il nuovo file salvato su Google Drive Cloud:
-	res.redirect( res.locals.red_uri )
+	// res.redirect( res.locals.red_uri )
 });
 
-/*** FINE API ESTERNA ***/
 
 
 
 
 
 
-
-//inizio API ESTERNE
-/*** inizio API INTERNE ***/
+/********************************************/
+/*				  API INTERNE	 		    */
+/********************************************/
 
 // https://apidocjs.com/#example-full
 // https://apidocjs.com/example/
@@ -646,70 +645,15 @@ app.post("/analisi/filtri/tabella", function(req,res){
 	else res.status(200).send('oh no')
 })
 
-// per aggiornare la documentazione: ' apidoc -e public -o public/apidoc '
-
-
-
-
-//fine API ESTERNE 
+// per aggiornare la documentazione: ' apidoc -i server/ -e server/public/ -o server/public/apidoc/ '
 
 
 
 
 
-
-
-
-app.post("/deleteriga", async(req,res)=> {
-	var prova = JSON.stringify(req.body);
-	var prova2 = JSON.parse(prova);
-	const pool = new Pool({
-		user: prova2.USER,
-		host: prova2.HOST,
-		database: prova2.DB,
-		password: prova2.PASS,
-		port: prova2.PORT,
-
-	});
-
-	var listaCampi= prova2.LISTACAMPI
-	var listaValori= prova2.LISTAVALORI
-	var query = "delete from " +  prova2.TABELLA + " where "
-	var t = 0
-	if( listaValori== undefined || listaValori == null || listaValori.length==0 ){
-		pool.end(function (err) {
-			console.log(err);
-		});
-		
-		res.send("errore")
-	    return 
-}
-	else
-	{listaCampi.forEach(element => {
-		query = query + element + " = '" + listaValori[t] + "' and "
-		t++
-	
-	})};
-
-	query = query.substring(0,query.length-4)
-	
-	pool.query(query, (error, results, fields) => {
-		if (error) {
-			res.send(error);
-			pool.end(function (err) {
-				console.log(err);
-				return;
-			});
-		} else {
-			res.send('ok');
-			pool.end(function (err) {
-				console.log(err);
-				return;
-			});
-		}
-	});
-
-})
+/********************************************/
+/*				  	LOGIN		 		    */
+/********************************************/
 
 //const utentelist =  utente.list()
 app.post('/registrazione', async (req, res) => {
@@ -761,17 +705,7 @@ app.post('/registrazione', async (req, res) => {
 
 /*** GOOGLE LOGIN ***/
 
-// app.get('/users/auth/google_oauth2', function(req,res){
-//   googleOAuth.Google_RequestCode(req,res)
-// })
-
-// app.get('/users/auth/google_oauth2/callback', function(req,res){
-//   var google_authcode = req.query.code;
-//   googleOAuth.Google_GetToken(req,res, google_authcode);
-// })
-
-app.get(
-	'/users/auth/google_oauth2',
+app.get( '/users/auth/google_oauth2',
 	googleOAuth.Google_RequestCode, //middleware function, allora ha come parametri "(req,res,next)"
 	function already_Authenticated(req, res) {
 		res.redirect('/static/home/');
@@ -825,23 +759,6 @@ app.get('/users/auth/google_oauth2/callback', googleOAuth.Google_GetToken, async
 	// res.redirect('/static/home/');
 });
 
-// app.get('/elimina', function (req, res) {
-// 	res.clearCookie('googleLogin');
-// 	res.redirect('/');
-// 	res.end();
-// });
-
-// app.get('/test', function (req, res) {
-// 	res.send(
-// 		"Benvenuto/a!<br> <img src='" +
-// 			req.cookies.googleLogin.propic +
-// 			"' atl=''/>" +
-// 			req.cookies.googleLogin.name +
-// 			'<br><br><br>Questo cookie durerà fino a ' +
-// 			req.cookies.googleLogin.expire_time +
-// 			'<br>per sloggarti: <button onclick=\'location.href = "/elimina"\';>log out</button>'
-// 	);
-// });
 
 /*
 da: per esempio dall'index.html tramite una form) href (nè get nè post.. in teoria get, al massimo..)
@@ -861,10 +778,13 @@ ora da app.js posso usaree il token per il mio obiettivo
 /*** fine GOOGLE LOGIN ***/
 
 
-app.post('/updatecookie', async (req, res) => {
-	res.cookie('datiUtente', { errore: false });
-	res.redirect('/static/login/');
-});
+
+
+
+// app.post('/updatecookie', async (req, res) => {
+// 	res.cookie('datiUtente', { errore: false });
+// 	res.redirect('/static/login/');
+// });
 
 app.post('/autenticazione', async (req, res) => {
 	var prova3 = JSON.stringify(req.body);
@@ -896,99 +816,13 @@ app.post('/autenticazione', async (req, res) => {
 		}
 	});
 });
-/* AUTENTICAZIONE */
-app.post('/autenticazione2', function (req, res) {
-	//var utility= ""
-	var options = {
-		url: 'http://admin:admin@' + process.env.COUCHDB_URL + '/persona/_find',
 
-		method: 'PUT',
-		headers: {
-			'Content-Type': 'application/json',
-			'Accept-Charset': 'utf-8',
-		},
-		body: {
-			selector: {
-				name: '"Paolo"',
-			},
-			fields: ['sesso'],
-			limit: 2,
-			skip: 0,
-		},
-		timeout: 5000,
-		json: true,
-	};
-	request.post(options, (err, body) => {
-		//var prova =  JSON.stringify(req.body)
-		//var dati =  JSON.parse(prova)
 
-		if (err) {
-			console.log('errore');
-			res.send('non ok ' + err);
-		} else {
-			//var dati2 = JSON.parse(body)
 
-			res.send('ok' + body);
-		}
-	});
-});
 
-/*app.post('/autenticazione', function(req,res){
-  var utility= ""
-  request.get('http://admin:admin@'+ process.env.COUCHDB_URL+"/persona"+"/_all_docs?include_docs=true", function(error,result,body)
-  {
-    var prova =  JSON.stringify(req.body) 
-    dati =  JSON.parse(prova)
-    
-     if(error)
-     {
-       console.log("errore")
-       res.send(error)
-     }
-     else{
-      
-      var dati2 = JSON.parse(body)
-      var strin = ""
-     
-      res.send(dati2.rows[1].doc.name+" " + body + " " + dati.username)
-      
-     }
-  });
-
-   
-  
-
-//  var t = prova.password 
-  //var c = prova.username 
- 
-})*/
-/*
-  if (valutazione=="pessima") {
-    res.send("Ciao " + req.body.username + "!<br> Certo che la tua password <code>"+req.body.password+"</code> è davvero " + valutazione + "!");
-  } else {
-    res.redirect('http://localhost:8080/nginx');
-  }
-  */
-
-/* COUCHDB */
-
-// var  options = {
-//   url: 'http://admin:admin@'+ process.env.COUCHDB_URL+'/utente/10',
-//   method: 'POST',
-//   headers: {
-//      'Content-Type': 'application/json',
-//      'Accept-Charset': 'utf-8'
-
-//    },
-//   body: {
-//          "name": "nome",
-//          "cognome": "cognome",
-//          "eta":21,
-//          "sesso": "no",
-//          "codice-fiscale" : "AAAABBBB"
-//         },
-//   json: true
-// };
+/********************************************/
+/*				 utils COUCHDB	 		    */
+/********************************************/
 
 app.get('/insutente', function (req, res) {
 	var options = {
@@ -1120,207 +954,63 @@ app.get('/eliminadb', function (req, res) {
 	});
 });
 
-/* request({
-    url: 'http://admin:admin@'+ process.env.COUCHDB_URL,
-    
-    method: 'GET',
-  
-  }, function(error, response, body){
-    if(error) {
-      console.log(error + "AOOOO: http://admin:admin@"+ process.env.COUCHDB_URL);
-      res.send(response.statusCode+"AOOOO: http://admin:admin@"+ process.env.COUCHDB_URL);
-    } else {
-      res.send(response.statusCode+" "+body)
-      console.log(response.statusCode, body);
-    }
-  }); */
-
-app.get('prova', function (req, res) {
-	request(
-		{
-			url: 'http://admin:admin@' + process.env.COUCHDB_URL + '/utente/4',
-
-			method: 'POST',
-
-			body: {
-				nome: 'ciao',
-				cognome: 'prova',
-			},
-			content_type: 'application/json',
-		},
-		function (error, response, body) {
-			if (error) {
-				console.log(error + 'AOOOO: http://admin:admin@' + process.env.COUCHDB_URL);
-				res.send(response.statusCode + 'AOOOO: http://admin:admin@' + process.env.COUCHDB_URL);
-			} else {
-				res.send(response.statusCode + ' ' + body);
-				console.log(response.statusCode, body);
-			}
-		}
-	);
-});
-
-/* fine COUCHDB */
 
 
-app.get('/prima_risorsa_get', function (req, res) {
-	res.send('sono la prima risorsa GET su questo server');
-});
 
-app.get('/stessa_risorsa', function (req, res) {
-	res.send('sono la stessa_risorsa acceduta in GET');
-});
-app.post('/stessa_risorsa', function (req, res) {
-	res.send('sono la stessa_risorsa acceduta in POST');
-});
+/********************************************/
+/*				   HOME DB		 		    */
+/********************************************/
 
-app.get('/seconda_risorsa_get', function (req, res) {
-	res.send('sono la seconda risorsa GET su questo server');
-});
-
-app.get('/stato_server', function (req, res) {
-	res.send('nodo: ' + process.env.NODE_ENV + ' \nistanza: ' + process.env.INSTANCE + ' \nporta: ' + process.env.PORT);
-});
-
-app.post('/prima_risorsa_post', function (req, res) {
-	res.send('sono la prima risorsa POST su questo server');
-});
-
-/* LOGIN (form) */
-
-app.post('/orario', function (req, res) {
-	// per reindirizzare su risorse statiche che il server conosce:
-	//res.redirect('http://localhost:8080/static/index.html');
-
-	// per comunicare con API:
-	/*request({
-    uri: 'http://localhost:8080/static/index.html',
-    method: 'GET'
-  }, function(error, response, body){
-    if (error){
-      console.log(error);
-    } else {
-      res.send(response.statusCode+" "+body);
-      console.log(response.statusCode, body);
-    }
-  })*/
-
-	// per fare operazioni di logica:
-	/*var valutazione = "null";
-  JSON.stringify(req.body.myPassword).length<8 ? valutazione="pessima" : valutazione="ottima";
-  res.send("Ciao " + req.body.myEmail + "!<br> Certo che la tua password <code>"+req.body.myPassword+"</code> è davvero " + valutazione + "!");*/
-	//res.status(301).redirect("https://www.google.com"); //per reindirizzare su un altro URL. ma non è possibile metterlo dopo "res.send"
-	//console.log("POST "+req.body.myEmail+" and "+req.body.myPassword);
-
-	// per fare operazioni di logica e reindirizzare con altre informazioni in più:
-	res.send(JSON.stringify(req.body));
-	let valutazione;
-	JSON.stringify(req.body.password).length < 8 ? (valutazione = 'pessima') : (valutazione = 'ottima');
-	if (valutazione == 'pessima') {
-		res.send(
-			'Ciao ' +
-				req.body.username +
-				'!<br> Certo che la tua password <code>' +
-				req.body.password +
-				'</code> è davvero ' +
-				valutazione +
-				'!'
-		);
-	} else {
-		res.redirect('http://localhost:8080/nginx');
-	}
-});
-
-/* fine LOGIN (form) */
-
-// function inserisciQualcosa(db)
-// {
-
-//   var  options = {
-//     url: 'http://admin:admin@'+ process.env.COUCHDB_URL+'/persona/'+req.query.numero,
-//     method: 'PUT',
-//     headers: {
-//        'Content-Type': 'application/json',
-//        'Accept-Charset': 'utf-8'
-
-//      },
-//     body: {
-//            "name": req.query.nome,
-//            "cognome": req.query.cognome,
-//            "eta":parseInt(req.query.eta),
-//            "sesso": req.query.sesso,
-//            "codice-fiscale" : req.query.codf
-//           },
-//     json: true
-//   };
-
-//   request.put(options, (err, body) => {
-
-//     if (err) {
-//       res.send("no " +err)
-//     }
-//     //console.log(`Status: ${res.statusCode}`);
-//     //res.write(response.statusCode.toString());
-//     res.send("ok "+body.toJSON.toString+ req.query.numero)
-//   });
-
-// }
-
-app.get('/prova', function (req, res) {
-	request.get(
-		'http://admin:admin@' + process.env.COUCHDB_URL + '/' + req.query.db + '/_all_docs?include_docs=true',
-		function (error, result, body) {
-			if (error) {
-				res.send('non ok' + error);
-			} else {
-				//var prova = JSON.stringify(body)
-				var dati = JSON.parse(body);
-				//var t = dati.offset
-				res.send(dati.total_rows + '');
-			}
-		}
-	);
-});
-
-app.get('/prova2', async (req, res) => {
-	const pool = new Pool({
-		user: 'postgres',
-		host: 'postgres',
-		database: 'utente',
-		password: 'adminpass',
-		port: '5432',
-	});
-
-	pool.query("select table_name from information_schema.tables where table_schema = 'public'", function (err, res2) {
-		if (err) res.send(err);
-		else res.send(res2);
-	});
-	pool.end(function (err) {
-		console.log(err);
-	});
-});
-
-app.post('/colonneTabella', async (req, res) => {
-	const pool = new Pool({
-		user: 'postgres',
-		host: 'postgres',
-		database: 'utente',
-		password: 'adminpass',
-		port: '5432',
-	});
-
+app.post("/deleteriga", async(req,res)=> {
 	var prova = JSON.stringify(req.body);
 	var prova2 = JSON.parse(prova);
-	pool.query(
-		"select column_name from information_schema.columns where table_name = '" + prova2.tabella + "'",
-		function (err, res2) {
-			res.send(res2);
-		}
-	);
-	pool.end(function (err) {
-		console.log(err);
+	const pool = new Pool({
+		user: prova2.USER,
+		host: prova2.HOST,
+		database: prova2.DB,
+		password: prova2.PASS,
+		port: prova2.PORT,
+
 	});
-});
+
+	var listaCampi= prova2.LISTACAMPI
+	var listaValori= prova2.LISTAVALORI
+	var query = "delete from " +  prova2.TABELLA + " where "
+	var t = 0
+	if( listaValori== undefined || listaValori == null || listaValori.length==0 ){
+		pool.end(function (err) {
+			console.log(err);
+		});
+		
+		res.send("errore")
+	    return 
+	}
+	else
+	{listaCampi.forEach(element => {
+		query = query + element + " = '" + listaValori[t] + "' and "
+		t++
+	
+	})};
+
+	query = query.substring(0,query.length-4)
+	
+	pool.query(query, (error, results, fields) => {
+		if (error) {
+			res.send(error);
+			pool.end(function (err) {
+				console.log(err);
+				return;
+			});
+		} else {
+			res.send('ok');
+			pool.end(function (err) {
+				console.log(err);
+				return;
+			});
+		}
+	});
+
+})
 
 app.post('/selezionaDatiTabella', async (req, res) => {
 	const pool = new Pool({
@@ -1343,13 +1033,6 @@ app.post('/selezionaDatiTabella', async (req, res) => {
 });
 
 app.post('/connessionedb', async (req, res) => {
-	/*const pool = new Pool({
-        user: "postgres",
-        host: "postgres",
-        database: "utente",
-        password: "adminpass",
-        port: "5432"
-      })*/
 	var prova = JSON.stringify(req.body);
 	var prova2 = JSON.parse(prova);
 
@@ -1388,13 +1071,7 @@ app.post('/selezionaDatiTabelladinamico', async (req, res) => {
 		password: prova2.PASS,
 		port: prova2.PORT,
 	});
-	/*const pool = new Pool({
-            user: "postgres",
-            host: "postgres",
-            database: "utente",
-            password: "adminpass",
-            port: "5432"
-          });*/
+
 	pool.query('select * from  ' + prova2.tabella + ' limit 100', function (err, res2) {
 		res.send(res2);
 	});
@@ -1413,13 +1090,6 @@ app.post('/tabelledb', async (req, res) => {
 		password: prova2.PASS,
 		port: prova2.PORT,
 	});
-	/*const pool = new Pool({
-              user: "postgres",
-              host: "postgres",
-              database: "utente",
-              password: "adminpass",
-              port: "5432"
-            });*/
 
 	pool.query("select table_name from information_schema.tables where table_schema = 'public'", function (err, res2) {
 		if (err) {
@@ -1451,7 +1121,6 @@ app.post('/insertriga', async (req, res) => {
 	var pass = 0;
 	var out = '';
 	for (var key in prova2) {
-		//var key2 = toString(key)
 
 		if (key != 'HOST' && key != 'DB' && key != 'PORT' && key != 'PASS' && key != 'USER') {
 			if (pass < 2) {
@@ -1467,7 +1136,7 @@ app.post('/insertriga', async (req, res) => {
 		}
 	}
 	var query = 'INSERT INTO ' + prova2.tabella + '(' + campi + ')' + 'VALUES' + '(' + valori + ');' + out;
-	//res.send(query)
+
 	pool.query(query, async (error) => {
 		if (error) {
 			res.send(error + prova2.DB);
@@ -1562,16 +1231,24 @@ app.post('/updatedbutente', async (req, res) => {
 });
 
 
-app.get('/pagina_dati', (req,res) => {
-	
+
+
+
+app.get('/stato_server', function (req, res) {
+	res.send('nodo: ' + process.env.NODE_ENV + ' \nistanza: ' + process.env.INSTANCE + ' \nporta: ' + process.env.PORT);
+});
+
+app.post('/prima_risorsa_post', function (req, res) {
+	res.send('sono la prima risorsa POST su questo server');
+});
+
+app.get('/', (req,res) => {
+	res.redirect('/static/');
 })
 
 
-  
-server.listen( 8080, () => {
+server.listen(process.env.PORT || 8080, () => {
     console.log('Application server ')
 });
 
-module.exports = server
-
-
+module.exports = server // for testing
